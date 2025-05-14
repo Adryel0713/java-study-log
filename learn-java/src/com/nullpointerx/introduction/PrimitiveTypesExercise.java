ead7cb0afa40f559b7f044ea73d3404fdb319991
Adryel0713 <clauber.adriel@gmail.com>
2025-05-13 23:14:31 -0300

    Exercicio sobre tipos primitivos(PrimitiveTypesExercise

---
+++ learn-java/src/com/nullpointerx/introduction/PrimitiveTypesExercise.java
@@ -0,0 +1,22 @@
+package com.nullpointerx.introduction;
+
+public class PrimitiveTypesExercise {
+    /**
+     *
+     * int, double, float, char, byte, short, long, boolean
+     */
+    public static void main(String[] args) {
+    String name = "Adryel";
+    String address = "na rua teste onde eu sei la";
+    double salary = 1000.0;
+    String date = "5/13/2025";
+    String relatorio = "Eu " + name + ", morando no endereço " + address
+            + ", confirmo que recebi o salario de "+ salary + ", na data " + date;
+
+    System.out.println(relatorio);
+    /*
+    System.out.println("Eu " + name + ", morando no endereço " + address
+            + ", confirmo que recebi o salario de "+ salary + ", na data " + date);
+     */
+    }
+}
